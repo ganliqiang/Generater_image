b3d8cbe99306c21d9034ac31be55bac2f229b205
ganliqiang <898926172@qq.com>
2018-07-18 17:27:37 +0800

    the third commit

---
+++ gen_image.py
@@ -0,0 +1,75 @@
+import os, sys
+sys.path.append(os.getcwd())
+
+import time
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.datasets
+import tensorflow as tf
+
+import tflib as lib
+import tflib.ops.linear
+import tflib.ops.conv2d
+
+import tflib.ops.batchnorm
+import tflib.ops.deconv2d
+import tflib.save_images
+import tflib.mnist
+import tflib.plot
+MODE = 'wgan-gp' # dcgan, wgan, or wgan-gp
+DIM = 64 # Model dimensionality
+BATCH_SIZE = 50 # Batch size
+CRITIC_ITERS = 5 # For WGAN and WGAN-GP, number of critic iters per gen iter
+LAMBDA = 10 # Gradient penalty lambda hyperparameter
+ITERS = 200000 # How many generator iterations to train for 
+OUTPUT_DIM = 784 # Number of pixels in MNIST (28*28)
+def Generator(n_samples, noise=None):
+    if noise is None:
+        noise = tf.random_normal([n_samples, 128])
+
+    output = lib.ops.linear.Linear('Generator.Input', 128, 4*4*4*DIM, noise)
+    if MODE == 'wgan':
+        output = lib.ops.batchnorm.Batchnorm('Generator.BN1', [0], output)
+    output = tf.nn.relu(output)
+    output = tf.reshape(output, [-1, 4*DIM, 4, 4])
+
+    output = lib.ops.deconv2d.Deconv2D('Generator.2', 4*DIM, 2*DIM, 5, output)
+    if MODE == 'wgan':
+        output = lib.ops.batchnorm.Batchnorm('Generator.BN2', [0,2,3], output)
+    output = tf.nn.relu(output)
+
+    output = output[:,:,:7,:7]
+
+    output = lib.ops.deconv2d.Deconv2D('Generator.3', 2*DIM, DIM, 5, output)
+    if MODE == 'wgan':
+        output = lib.ops.batchnorm.Batchnorm('Generator.BN3', [0,2,3], output)
+    output = tf.nn.relu(output)
+
+    output = lib.ops.deconv2d.Deconv2D('Generator.5', DIM, 1, 5, output)
+    output = tf.nn.sigmoid(output)
+
+    return tf.reshape(output, [-1, OUTPUT_DIM])
+
+fixed_noise = tf.constant(np.random.normal(size=(128, 128)).astype('float32'))
+fixed_noise_samples = Generator(1, noise=fixed_noise)
+
+i=1
+
+def generate_image(frame):
+    samples = sess.run(fixed_noise_samples)
+    print samples.shape
+    lib.save_images.save_images(samples.reshape((128, 28, 28)), '1sam_{}.png'.format(i))
+
+
+with tf.Session() as sess:
+    for i in range(5):
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
+        saver.restore(sess, tf.train.latest_checkpoint('checkpoints'))
+        generate_image(1)
+        i=i+1
+
+    
